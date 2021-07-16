cfd5768593c291fc2d51bf4674e6481ddfe4e15f
iguardines <iguardines@gmail.com>
2021-07-16 17:48:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+DEBUG = True
+
+@app.errorhandler(404)
+def not_found(error):
+	return "Not Found"
+
+
+@app.route('/test', methods=['GET'])
+def index():
+	return {"mensaje":2}
+
+
+if __name__ == "__main__":
+	app.run(port=5000, debug= DEBUG)
